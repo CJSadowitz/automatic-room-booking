977a17e6132d6a3c1f4d76013ea315dc358e6c74
CJSadowitz <147172895+CJSadowitz@users.noreply.github.com>
2023-10-06 12:49:43 -0400

    Create Event_Names.py

---
+++ Event_Names.py
@@ -0,0 +1,13 @@
+# Cursed Text?
+import random
+
+eventNames = [
+  'some event name',
+  'another event name'  
+]
+
+specialEventNames = [
+  'some special event name',
+  'another special event name'
+]
+a = int(random.random()*100) % len(eventNames) # randomly get a name from eventNames
